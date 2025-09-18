27b453e78da497ea7130ba17555f381e4d7c90c5
Carlos E. Feliz <NotAnEmail6_9@protonmail.com>
2025-09-18 16:00:03 -0400

    Update db.js

--- netlify/functions/db.js
+++ netlify/functions/db.js
@@ -1,18 +1,11 @@
 import pkg from "pg";
 const { Pool } = pkg;
 
-let sslConfig = false;
-
-if (process.env.DB_CA_CERT) {
-  sslConfig = {
-    rejectUnauthorized: true,
-    ca: process.env.DB_CA_CERT.replace(/\\n/g, "\n"), // 👈 convierte los \n en saltos reales
-  };
-}
-
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: sslConfig,
+  ssl: {
+    rejectUnauthorized: false, // necesario para Aiven
+  },
 });
 
 export default pool;
