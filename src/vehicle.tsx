f6cfac221751f2e6d02c3abf942d48638f3f525c
Carlos E. Feliz <NotAnEmail6_9@protonmail.com>
2025-11-15 21:58:01 -0400

    update date

--- src/vehicle.tsx
+++ src/vehicle.tsx
@@ -29,7 +29,7 @@ export default function DocumentoVehiculo() {
     const day = String(d.getDate()).padStart(2, "0");
     const month = String(d.getMonth() + 1).padStart(2, "0");
     const year = d.getFullYear();
-    return `${day}/${month}/${year}`;
+    return `${month}/${day}/${year}`;
   };
   useEffect(() => {
     axios
